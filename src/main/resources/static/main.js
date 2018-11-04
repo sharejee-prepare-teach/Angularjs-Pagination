72ba6549330bc4672b9402963d41bf9604858ae3
Rith <rithronlkh@gmail.com>
2018-11-04 18:58:04 +0700

    add ng-grid pagination not yet done

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -102,18 +102,99 @@ app.controller("StudentController", function($scope, $http) {
         $scope.studentForm.stuSex = "";
         $scope.studentForm.address = "";
     };
+
+    $scope.filterOptions = {
+
+        //text you want to search will be put inside the filterText
+        filterText: ""
+    };
+
+    $scope.pagingOptions = {
+        //no of records that need to be displayed per page will be depend on pagesizes
+        pageSizes: [1,2,3],
+        pageSize: 1,
+        //this is for the page no that is selected
+        currentPage: 1
+    };
+    //this is the method that is used to call the server and bring back the data in nggrid
+    $scope.getPagedDataAsync = function (pageSize, page, searchText) {
+        setTimeout(function () {
+            var data;
+            var page = $scope.pagingOptions.currentPage;
+            var pageSize = $scope.pagingOptions.pageSize;
+
+            //if filter text is there then this condition will execute
+            if (searchText) {
+                var ft = searchText.toLowerCase();
+                $http({
+                    method:'GET',
+                    url:'/students',
+                    data:{currentPage:page,pageSize:pageSize}
+                }).then(
+                    function(largeLoad) { // success
+                        //with data must send the total no of items as well
+                        $scope.totalServerItems=largeLoad.data.totalElement;
+                        //here's the list of data to be displayed
+                        data = largeLoad.data.list.filter(function(item) {
+                            return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                        });
+                        $scope.setPagingData(data,page,pageSize);
+                    },
+                    function(res) { // error
+                        console.log("Error: " + res.status + " : " + res.data);
+                    }
+                );
+            }
+            else
+            {
+                $http({
+                    method:'GET',
+                    url:'/students',
+                    data:{currentPage:page,pageSize:pageSize}
+                }).then(
+                    function(largeLoad) { // success
+                        $scope.totalServerItems=largeLoad.data.totalElement;
+                        $scope.setPagingData(largeLoad.data.list,page,pageSize);
+                    },
+                    function(res) { // error
+                        console.log("Error: " + res.status + " : " + res.data);
+                    }
+                );
+            }
+        },100);
+    };
+    //according to the data coming from server side,pagination will be set accordingly
+    $scope.setPagingData = function(data, page, pageSize){
+        $scope.myData = data;
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    };
+    //watch for pagination option.here pagingOptions will be watched each time value changes and then set the data accordingly
+    $scope.$watch('pagingOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal || newVal.currentPage !== oldVal.currentPage) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
+
+    $scope.$watch('filterOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
     $scope.gridOptions = {
         data: 'students',
-        enableRowSelection: false,
-        enableCellEditOnFocus: true,
-        multiSelect: false,
-        columnDefs: [
-            { field: 'stuId', displayName: 'stuId', enableCellEdit: false } ,
-            { field: 'stuName', displayName: 'stuName', enableCellEdit: false} ,
-            { field: 'stuSex', displayName: 'stuSex', enableCellEdit: true} ,
-            { field:'', displayName: 'Save', enableCellEdit: false,
-                cellTemplate: '<button id="editBtn" type="button"  ng-click="saveItem(row.entity.name, row.entity.surname,row.entity.address)" >Save</button>'}
-        ]
+        enablePaging: true,
+        showFooter: true,
+        totalServerItems: 'totalServerItems',
+        pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions,
 
+        //field : id is the key from the data coming from server side
+        columnDefs:[
+            {displayName:'stuId',field:'stuId'},
+            {displayName:'stuName',field:'stuName'},
+            {displayName:'stuSex',field:'stuSex'},
+        ]
     };
 });
