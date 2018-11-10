ddc5cc614b318c7b204ef303b0a3e2e7c659f064
Rith <rithronlkh@gmail.com>
2018-11-10 12:20:29 +0700

    add refector

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,27 +1,23 @@
 var app = angular.module('myApp', ['ui.router']);
 
 app.config(function($stateProvider){
-    $stateProvider.state('news', {
-        url: '/newst',
-        templateUrl : 'newst'
-    })
-});
-
-app.config(function($stateProvider){
-    $stateProvider.state('news.sports', {
-        url: '/sportst',
-        templateUrl : 'sportst'
-    })
-});
-
-app.config(function($stateProvider){
-    $stateProvider.state('news.music', {
-        url: '/musict',
-        templateUrl : 'musict'
-    })
+    $stateProvider
+        .state('news', {
+            url: '/newst',
+            templateUrl : 'newst'
+        })
+        .state('news.sports', {
+            url: '/sportst',
+            templateUrl : 'sportst'
+        })
+        .state('news.music', {
+            url: '/musict',
+            templateUrl : 'musict'
+        })
 });
 
 app.controller('submenuController', function ($scope, $rootScope, $state) {
+    alert("ctl");
     $scope.showSubMenu = true;
 
     $scope.$watch(function(){
